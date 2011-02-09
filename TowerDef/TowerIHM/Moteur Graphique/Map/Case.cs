020cba2e43d0b6143accd97db39b320f4b749ffe
SND\Jason_Marechal_cp <SND\Jason_Marechal_cp@83a7461f-ea85-4138-920c-f536d31b1086>
2011-02-09 09:38:47 +0000

    Re-architechturage pour utilisation de classe partiel
    git-svn-id: https://xna3dtower.svn.codeplex.com/svn@1490 83a7461f-ea85-4138-920c-f536d31b1086

---
+++ TowerDef/TowerIHM/Moteur Graphique/Map/Case.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GlobalsComponent;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MoteurGraphique.Map
+{
+    partial class Case : IDrawable
+    {
+        Texture2D texture;
+
+        Vector2 Position
+        {
+            get;
+            private set;
+        }
+
+        public void Initialize(ref Texture2D tex)
+        {
+            texture = tex;
+            Position = new Vector2(X, Y);
+        }
+
+        public void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GLOBALS_GAME_RESSOURCES.GRAPHICS.GraphicsDevice.Viewport = GLOBALS_GAME_RESSOURCES.VP_viewGame;
+            GLOBALS_GAME_RESSOURCES.SPRITEBATCH.Draw(texture, Position, Color.White);
+            //throw new NotImplementedException();
+        }
+
+        public int DrawOrder
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> DrawOrderChanged;
+
+        public bool Visible
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> VisibleChanged;
+    }
+}
---
+++ TowerDef/TowerIHM/Moteur Graphique/Map/Terrain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Maquette1.Map
+{
+    public partial class Terrain : IDrawable
+    {
+        //Texture2D textureChemin;
+        //Texture2D textureConstructible;
+
+        public void Draw(GameTime gameTime)
+        {
+            foreach (Case c in Carte)
+            {
+                c.Draw(gameTime);
+            }
+
+            //throw new NotImplementedException();
+        }
+
+        public int DrawOrder
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> DrawOrderChanged;
+
+        public bool Visible
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> VisibleChanged;
+    }
+}
