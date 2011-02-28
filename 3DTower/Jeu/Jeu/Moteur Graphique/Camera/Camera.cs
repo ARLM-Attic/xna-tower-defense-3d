e2359beb8cbffec5a4828d5b78d421d23c8307fa
SND\Jason_Marechal_cp <SND\Jason_Marechal_cp@83a7461f-ea85-4138-920c-f536d31b1086>
2011-02-28 08:46:55 +0000

    git-svn-id: https://xna3dtower.svn.codeplex.com/svn@3311 83a7461f-ea85-4138-920c-f536d31b1086

---
+++ 3DTower/Jeu/Jeu/Moteur Graphique/Camera/Camera.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GlobalComponents;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Jeu.Moteur_Graphique.Camera
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class Camera : Microsoft.Xna.Framework.GameComponent
+    {
+        private Vector3 position;
+        private Vector3 lookAt;
+        private Matrix view;
+        private Matrix projection;
+        private float aspectRatio;
+        private float near;
+        private float far;
+
+        public float OffsetX { get; set; }
+
+        public float OffsetY { get; set; }
+
+        public float OffsetZ { get; set; }
+
+        public float RotatioX { get; set; }
+
+        public float RotatioY { get; set; }
+
+        public float RotatioZ { get; set; }
+
+        public Camera(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+        }
+
+        public Vector3 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Vector3 LookAt
+        {
+            get { return lookAt; }
+            set { lookAt = value; }
+        }
+
+        public Matrix View
+        {
+            get { return view; }
+        }
+
+        public Matrix ProjectionMatrix
+        {
+            get { return projection; }
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+            near = 1.0F;
+            far = 2000;
+            aspectRatio =
+                GLOBALS_GAME_RESSOURCES.VP_viewGame.Width /
+                GLOBALS_GAME_RESSOURCES.VP_viewGame.Height;
+            projection = Matrix.CreatePerspectiveFieldOfView(
+                                        MathHelper.ToRadians(45.0f),
+                                        aspectRatio,
+                                        near,
+                                        far);
+            view = Matrix.Identity;// *Matrix.CreateRotationY(MathHelper.ToRadians(90));
+            Position = new Vector3(0, 200, 0);
+            LookAt = new Vector3(0, -10, 0);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Left))
+                OffsetX -= 10;
+
+            if (kstate.IsKeyDown(Keys.Right))
+                OffsetX += 10;
+
+            if (kstate.IsKeyDown(Keys.Down) && !kstate.IsKeyDown(Keys.LeftControl))
+                OffsetZ -= 10;
+
+            if (kstate.IsKeyDown(Keys.Up) && !kstate.IsKeyDown(Keys.LeftControl))
+                OffsetZ += 10;
+
+            if (kstate.IsKeyDown(Keys.Up) && kstate.IsKeyDown(Keys.LeftControl))
+                OffsetY += 10;
+
+            if (kstate.IsKeyDown(Keys.Down) && kstate.IsKeyDown(Keys.LeftControl))
+                OffsetY -= 10;
+
+            Position = new Vector3(OffsetX, OffsetY, OffsetZ);
+            LookAt = new Vector3(OffsetX, -10, OffsetZ);
+
+            view =
+                Matrix.CreateLookAt(position, lookAt, Vector3.Up) *
+                Matrix.CreateRotationY(MathHelper.ToRadians(90));
+
+            base.Update(gameTime);
+        }
+    }
+}
