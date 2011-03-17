5f82830bfa4434bfb5bd20f77092b1ab5bc69ccd
SND\salutmikey_cp <SND\salutmikey_cp@83a7461f-ea85-4138-920c-f536d31b1086>
2011-03-17 09:40:36 +0000

    git-svn-id: https://xna3dtower.svn.codeplex.com/svn@4122 83a7461f-ea85-4138-920c-f536d31b1086

---
+++ 3DTower/Jeu/Jeu/Architechture/Moteur Calcul/TimeEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jeu
+{
+    public class TimeEvent
+    {
+        List<IAction> ls;
+        private int i;
+
+        public TimeEvent()
+        {
+            i = 0;
+            ls = new List<IAction>();
+        }
+
+        public void AjouterEvents(IAction action) 
+        {
+            ls.Add(action);
+        }
+
+        public void SupprimerEvents(IAction action) 
+        {
+            ls.Remove(action);
+        }
+
+        public void Action() 
+        {
+            foreach (IAction a in ls)
+            {
+                a.Action();
+            }
+        }
+    }
+}
